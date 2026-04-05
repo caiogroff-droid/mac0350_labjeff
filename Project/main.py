51ebe47f080fb157ee30c7f79706a88221b4607a
caiogroff-droid <caio.groff@gmail.com>
2026-04-05 15:57:43 -0300

    adicionando filtros

--- Project/main.py
+++ Project/main.py
@@ -68,13 +68,26 @@ def root(request: Request):
     return templates.TemplateResponse(request, "index.html")
     
 @app.post("/novoJogo", response_class=HTMLResponse)
-def criar_jogo(nome: str = Form(...), descricao: str = Form(None), nota: int = Form(None)):
+def criar_jogo(
+    nome: str = Form(...), 
+    descricao: str = Form(None), 
+    nota: int = Form(None),
+    plataforma_id: int = Form(...)  
+):
     with Session(engine) as session:
         novo_jogo = Jogo(nome=nome, descricao=descricao, nota=nota)
         session.add(novo_jogo)
         session.commit()
         session.refresh(novo_jogo)
-        return HTMLResponse(content=f"<p>✅ {novo_jogo.nome} foi adicionado!</p>")
+        
+        disponibilidade = Disponibilidade(
+            jogo_id=novo_jogo.id,
+            plataforma_id=plataforma_id
+        )
+        session.add(disponibilidade)
+        session.commit()
+        
+        return HTMLResponse(content=f"<p>✅ {novo_jogo.nome} foi adicionado à plataforma!</p>")
     
 @app.delete("/deletaJogo", response_class=HTMLResponse)
 def deletar_jogo(id: int = Form(...)):
@@ -102,6 +115,7 @@ def atualizar_jogo(
     id: int = Form(...), 
     nome: str = Form(None),
     descricao: str = Form(None),
+    plataforma_id: int = Form(None),
     nota: int = Form(None)
 ):
     with Session(engine) as session:
@@ -117,6 +131,13 @@ def atualizar_jogo(
             jogo.descricao = descricao
         if nota:
             jogo.nota = nota
+        if plataforma_id:
+            disponibilidade = Disponibilidade(
+                jogo_id=jogo.id,
+                plataforma_id=plataforma_id
+            )
+            session.add(disponibilidade)
+
             
         session.add(jogo)
         session.commit()
@@ -142,6 +163,16 @@ def deletar_plataforma(id: int = Form(...)):
         session.delete(plataforma)
         session.commit()
         return HTMLResponse(content=f"<p>O(a) plataforma {plataforma.nome} foi deletado(a)!</p>")
+    
+@app.delete("/deletaTodasPlataformas", response_class=HTMLResponse)
+def deletar_todas_plataformas():
+    with Session(engine) as session:
+        query = select(Plataforma)
+        plataformas = session.exec(query).all()
+        for plataforma in plataformas:
+            session.delete(plataforma)
+        session.commit()
+        return HTMLResponse(content=f"<p>Todas as plataformas foram deletadas!</p>")
 
 @app.put("/atualizaPlataforma", response_class=HTMLResponse)
 def atualizar_plataforma(
@@ -169,20 +200,50 @@ def atualizar_plataforma(
         session.refresh(plataforma)
         return HTMLResponse(content=f"<p>✅ {plataforma.nome} foi atualizado!</p>")
 
+def order(filtro):
+    if filtro == 'nota':
+        return Jogo.nota.desc()
+    elif filtro == 'nome':
+        return Jogo.nome
+    elif filtro == 'plataforma':
+        return Jogo.plataforma.any(Plataforma.nome)
 
-def buscar_jogos(busca):
+def buscar_jogos(busca, filtro):
     with Session(engine) as session:
-        query = select(Jogo).where(col(Jogo.nome).contains(busca)).order_by(Jogo.nota.desc())  
-        return session.exec(query).all()
+        query = select(Jogo).where(col(Jogo.nome).contains(busca)).order_by(order(filtro))  
+        jogos = session.exec(query).all()
+        for jogo in jogos:
+            print(f"Jogo: {jogo.nome}, Plataformas: {[p.nome for p in jogo.plataforma]}")
+
+            plataforma_query = (
+                select(Plataforma)
+                .join(Disponibilidade, Disponibilidade.plataforma_id == Plataforma.id)
+                .where(Disponibilidade.jogo_id == jogo.id)
+            )
+            jogo.plataforma = session.exec(plataforma_query).all()
+            print(f"Jogo: {jogo.nome}, Plataformas: {[p.nome for p in jogo.plataforma]}")
+        return jogos
     
 def buscar_plataformas(busca):
     with Session(engine) as session:
         query = select(Plataforma).where(col(Plataforma.nome).contains(busca)).order_by(Plataforma.nome)  
-        return session.exec(query).all()
+        plataformas = session.exec(query).all()
+        for plataforma in plataformas:
+            print(f"Plataforma: {plataforma.nome}, Jogos: {[j.nome for j in plataforma.jogos]}")
+
+            jogo_query = (
+                select(Jogo)
+                .join(Disponibilidade, Disponibilidade.jogo_id == Jogo.id)
+                .where(Disponibilidade.plataforma_id == plataforma.id)
+            )
+            plataforma.jogos = session.exec(jogo_query).all()
+            print(f"Plataforma: {plataforma.nome}, Jogos: {[j.nome for j in plataforma.jogos]}")
+        return plataformas
+    
     
 @app.get("/lista", response_class=HTMLResponse)
-def lista(request: Request, busca: str | None=''):
-    jogos = buscar_jogos(busca)
+def lista(request: Request, busca: str | None='', filtro: str | None=''):
+    jogos = buscar_jogos(busca, filtro)
     return templates.TemplateResponse(request, "lista.html", {"jogos": jogos})
 
 
@@ -192,7 +253,12 @@ def busca(request: Request):
 
 @app.get("/adicionarJogo")
 def novoJogo(request: Request):
-    return templates.TemplateResponse(request, "options.html")
+    plataformas = buscar_plataformas('')
+    return templates.TemplateResponse(
+        request, 
+        "options.html", 
+        {"plataformas": plataformas}
+    )
 
 @app.get("/plataformas", response_class=HTMLResponse)
 def plataformas(request: Request, busca: str | None=''):
